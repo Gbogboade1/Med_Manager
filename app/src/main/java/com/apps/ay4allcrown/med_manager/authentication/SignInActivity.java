f7d709e555d4803cac18703e110e3f50d7a7847f
GBOGBOADE AYOMIDE <gbogboadeayomide@gmail.com>
2018-08-15 00:30:28 +0100

    commit after adding firebase to project

---
+++ app/src/main/java/com/apps/ay4allcrown/med_manager/authentication/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.apps.ay4allcrown.med_manager.authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apps.ay4allcrown.med_manager.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
+
